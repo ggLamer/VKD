55ba6c23c21c047f689b869033f95491eede9c38
Azamat <azamat201760@yandex.ru>
2021-07-22 22:05:35 +0500

    add watch app

---
+++ WatchApp/app/src/main/java/com/gglamer/watchapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.gglamer.watchapp
+
+import ViewPagerAdapter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(R.layout.activity_main)
+
+        val viewpager = findViewById<ViewPager>(R.id.viewpager)
+
+        viewpager.adapter = ViewPagerAdapter(supportFragmentManager)
+
+    }
+
+}
