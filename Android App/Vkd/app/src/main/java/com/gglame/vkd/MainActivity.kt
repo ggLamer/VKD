85577fe0685b01a211b096ea59a314fa8c9be9ad
Azamat <azamat201760@yandex.ru>
2021-07-23 16:39:59 +0500

    update watch app

--- Android App/Vkd/app/src/main/java/com/gglame/vkd/MainActivity.kt
+++ Android App/Vkd/app/src/main/java/com/gglame/vkd/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.gglame.vkd
 
-import android.graphics.Color
-import android.os.AsyncTask
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 import kotlinx.coroutines.*
--- WatchApp/app/src/main/java/com/gglamer/watchapp/ButtonsFragment.kt
+++ WatchApp/app/src/main/java/com/gglamer/watchapp/ButtonsFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class ButtonsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buttons, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_buttons, container, false)
+
+
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val connect  = view.findViewById<Button>(R.id.Connect)
+
+
+        connect.setOnClickListener {
+
+        }
+
+
+
     }
 
 }
