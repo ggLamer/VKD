35e30a0b2aee471aabbdfcf5ae934a56360cf306
Azamat <azamat201760@yandex.ru>
2021-07-28 14:23:22 +0500

    Update Watch App

--- WatchApp/app/src/main/java/com/gglamer/watchapp/MainActivity.kt
+++ WatchApp/app/src/main/java/com/gglamer/watchapp/MainActivity.kt
@@ -5,15 +5,49 @@ import ViewPagerAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var ip = "192.168.239.242"
+        var state = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val viewpager = findViewById<ViewPager>(R.id.viewpager)
         viewpager.adapter = ViewPagerAdapter(supportFragmentManager)
+
+
+        fun getparam() {
+            GlobalScope.launch {
+                while (true) {
+                    try {
+                        val temperature = URL("http://192.168.239.242:5000/temperature").readText()
+                        //val humidity = URL("http://$ip/humidity").readText()
+                        //val pressure = URL("http://$ip/pressure").readText()
+
+                        runOnUiThread(kotlinx.coroutines.Runnable {
+                            TempFragment.progress = temperature
+                        })
+                    }catch (e: java.net.SocketException){
+                        getparam()
+                        println(e)
+                        println("catch")
+                    }
+                }
+            }
+        }
+        if (state == 1){
+            getparam()
+            println(state)
+        }
+
 
     }
 
